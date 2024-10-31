2b731d6a00f27c6b360e3ea08f6b8378aaf3e436
PhilReact <philliplangmartinez@gmail.com>
2024-10-31 20:35:13 +0200

    added for local

--- electron/src/setup.ts
+++ electron/src/setup.ts
@@ -122,6 +122,7 @@ export class ElectronCapacitorApp {
         // Use preload to inject the electron varriant overrides for capacitor plugins.
         // preload: join(app.getAppPath(), "node_modules", "@capacitor-community", "electron", "dist", "runtime", "electron-rt.js"),
         preload: preloadPath,
+        webSecurity: false
       },
     });
     this.mainWindowState.manage(this.MainWindow);
