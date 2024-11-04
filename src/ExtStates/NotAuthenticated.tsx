07995f0330dc632f8965b7935adc346feedf3f9c
PhilReact <philliplangmartinez@gmail.com>
2024-11-04 11:32:16 +0200

    removed gif

--- src/ExtStates/NotAuthenticated.tsx
+++ src/ExtStates/NotAuthenticated.tsx
@@ -23,7 +23,7 @@ import { set } from "lodash";
 import { cleanUrl, isUsingLocal } from "../background";
 
 const manifestData = {
-  version: '2.2.0'
+  version: '0.2.0'
 }
 
 export const NotAuthenticated = ({
