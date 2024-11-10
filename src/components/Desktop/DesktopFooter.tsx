978dd7d995209b19b7f487b69e7c804a1b238adc
PhilReact <philliplangmartinez@gmail.com>
2024-11-10 20:52:05 +0200

    change label to group

--- src/components/Desktop/DesktopFooter.tsx
+++ src/components/Desktop/DesktopFooter.tsx
@@ -143,7 +143,7 @@ export const DesktopFooter = ({
         >
           <IconWrapper
             color="rgba(250, 250, 250, 0.5)"
-            label="Hubs"
+            label="Groups"
             selected={isGroups}
           >
             <HubsIcon
