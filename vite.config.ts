fbe876951d4116ccb86df5ee6c7d1d01b974034a
PhilReact <philliplangmartinez@gmail.com>
2025-03-04 17:27:19 +0200

    added pwa for desktop

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized'
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 
@@ -14,7 +15,34 @@ export default defineConfig({
     setupFiles: ['./src/test/setup.ts']
   },
   assetsInclude: ['**/*.wasm'], 
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), wasm(), topLevelAwait(), VitePWA({
+    registerType: 'prompt', 
+    manifest: {
+      name: 'Qortal Hub',
+      short_name: 'Hub',
+      description: 'Your easy access to the Qortal blockchain',
+      start_url: '/',
+      display: 'standalone',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: '/qortal192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/qortal.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workbox: {
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB limit
+      disableDevLogs: true, // Suppresses logs in development
+    },
+  })],
   optimizeDeps: {
     esbuildOptions: {
       plugins: [fixReactVirtualized],
