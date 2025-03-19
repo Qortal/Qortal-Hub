6cd175c1a9f28b50f0d3fb18e1373f8ee2a70030
PhilReact <philliplangmartinez@gmail.com>
2025-03-19 17:17:36 +0200

    update version

--- src/ExtStates/NotAuthenticated.tsx
+++ src/ExtStates/NotAuthenticated.tsx
@@ -28,7 +28,7 @@ import { GlobalContext } from "../App";
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 
 const manifestData = {
-  version: "0.5.2",
+  version: "0.5.3",
 };
 
 
