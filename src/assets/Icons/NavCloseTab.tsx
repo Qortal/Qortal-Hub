58eb704dcd6c49abf367db2fd9aac6ddd735b8a7
Nicola Benaglia <nico.benaz@gmail.com>
2025-04-20 16:06:04 +0200

    Set correct property

--- src/assets/Icons/NavCloseTab.tsx
+++ src/assets/Icons/NavCloseTab.tsx
@@ -32,14 +32,14 @@ export const NavCloseTab: React.FC<SVGProps> = ({
         stroke={theme.palette.text.primary}
         stroke-width="2"
         fill={setColor}
-        fill-opacity={setOpacity}
+        fillOpacity={setOpacity}
       />
       <path
         d="M11.3333 5.66675L5.66658 11.3334"
         stroke={theme.palette.text.primary}
         stroke-width="2"
         fill={setColor}
-        fill-opacity={setOpacity}
+        fillOpacity={setOpacity}
       />
     </svg>
   );
