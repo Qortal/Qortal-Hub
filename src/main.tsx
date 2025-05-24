cb86a9b89da89a29b72c09a5fa3efcdaef0aed88
Nicola Benaglia <nico.benaz@gmail.com>
2025-05-24 12:19:11 +0200

    Rename files and folders (uniform the case)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '../src/styles/index.css';
-import './messaging/messagesToBackground';
-import { MessageQueueProvider } from './MessageQueueContext.tsx';
+import './messaging/MessagesToBackground.tsx';
+import { MessageQueueProvider } from './messaging/MessageQueueContext.tsx';
 import { ThemeProvider } from './components/Theme/ThemeContext.tsx';
 import { CssBaseline } from '@mui/material';
 import './i18n/i18n.js';
