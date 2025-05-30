06f0d456771c1edef503a3ee5a859c2f10eb543c
Nicola Benaglia <nico.benaz@gmail.com>
2025-05-30 08:16:31 +0200

    Refactor property names

--- src/assets/Icons/NavCloseTab.tsx
+++ src/assets/Icons/NavCloseTab.tsx
@@ -30,14 +30,14 @@ export const NavCloseTab: React.FC<SVGProps> = ({
       <path
         d="M5.66675 5.66669L11.3334 11.3334"
         stroke={theme.palette.text.primary}
-        stroke-width="2"
+        strokeWidth="2"
         fill={setColor}
         fillOpacity={setOpacity}
       />
       <path
         d="M11.3333 5.66675L5.66658 11.3334"
         stroke={theme.palette.text.primary}
-        stroke-width="2"
+        strokeWidth="2"
         fill={setColor}
         fillOpacity={setOpacity}
       />
