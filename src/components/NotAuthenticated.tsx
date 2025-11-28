4e29ae8dd0929b6c51f9b7864e624068ed0d1866
PhilReact <philliplangmartinez@gmail.com>
2025-11-28 11:01:20 +0200

    update version

--- src/components/NotAuthenticated.tsx
+++ src/components/NotAuthenticated.tsx
@@ -42,7 +42,7 @@ import { useAuth } from '../hooks/useAuth.tsx';
 import { nodeDisplay } from '../utils/helpers.ts';
 
 export const manifestData = {
-  version: '0.5.6',
+  version: '0.5.7',
 };
 
 export const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
