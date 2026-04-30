b7279e878a526869ff06c2dee1dd910ea362fd34
PhilReact <philliplangmartinez@gmail.com>
2026-04-30 18:30:06 +0300

    remove clear sw

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,6 @@ import { MessageQueueProvider } from './messaging/MessageQueueContext.tsx';
 import { ThemeProvider } from './components/Theme/ThemeContext.tsx';
 import { CssBaseline } from '@mui/material';
 import './i18n/i18n.js';
-import { OnLaunchWrapper } from './OnLaunchWrapper.tsx';
-import { clearLocalPreviewServiceWorker } from './utils/clearLocalPreviewServiceWorker.ts';
-
-clearLocalPreviewServiceWorker();
 
 createRoot(document.getElementById('root')!).render(
   <>
